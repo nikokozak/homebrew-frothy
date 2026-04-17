5969e5c77efee688a2270cc57f2d1d7df7bf167e
Niko Kozak <nikokozak@gmail.com>
2026-04-17 16:59:57 -0400

    frothy 0.1.1

--- Formula/frothy.rb
+++ Formula/frothy.rb
@@ -1,32 +1,47 @@
 class Frothy < Formula
   desc "Live lexical language for programmable devices"
   homepage "https://github.com/nikokozak/frothy"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.0-darwin-arm64.tar.gz"
-      sha256 "59a94335479141c19b939985f867c46b4551361a32f1aedb012863f37d45d9e6"
+      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.1-darwin-arm64.tar.gz"
+      sha256 "f38e8360d4e4c74b8611f17ca24ad3b1b14365649a25c1b9cdec9fc96d1778ee"
     end
     on_intel do
-      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.0-darwin-amd64.tar.gz"
-      sha256 "fdaace413b5d798129eaa7bbbfc069bd56ecf96bae99c015d2128e7e80b887ca"
+      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.1-darwin-amd64.tar.gz"
+      sha256 "e221fcdde5d551ee97d9c4332361f70edca90bcd1885763cbb2cf8da3cc02980"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.0-linux-amd64.tar.gz"
-      sha256 "e144b6a1d0146f2432c5b328748181b883cf836067e1289b566db57b5ae98210"
+      url "https://github.com/nikokozak/frothy/releases/download/v#{version}/frothy-v0.1.1-linux-amd64.tar.gz"
+      sha256 "2dcc23a035565a2ee5dba05d7a7a7fbf4fdbcec1959cd6e58ada56fca83d1d92"
     end
   end
 
+  head do
+    url "https://github.com/nikokozak/frothy.git", branch: "main"
+    depends_on "go" => :build
+  end
+
   def install
-    bin.install "frothy"
+    if build.head?
+      cd "tools/cli" do
+        system "go", "run", "./internal/sdk/cmd/generate",
+               "-repo", buildpath.to_s,
+               "-out", "internal/sdk/generated"
+        system "go", "build", "-o", bin/"frothy", "."
+      end
+    else
+      bin.install "frothy"
+    end
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/frothy --version")
+    output = shell_output("#{bin}/frothy --version")
+    assert_match "frothy ", output
   end
 end
