3b0cdd03f55b4e8cef25a2747379b266cac84c4a
Niko Kozak <nikokozak@gmail.com>
2026-05-06 00:47:12 -0400

    point disabled formula at froth source

--- Formula/frothy.rb
+++ Formula/frothy.rb
@@ -1,9 +1,9 @@
 class Frothy < Formula
   desc "Legacy tap entry for Froth"
   homepage "https://frothlang.org"
-  url "https://github.com/nikokozak/frothy/archive/7533c8167ad591f01a7e1bcddda827536efdb9f7.tar.gz"
+  url "https://github.com/nikokozak/froth/archive/7533c8167ad591f01a7e1bcddda827536efdb9f7.tar.gz"
   version "0.1.0"
-  sha256 "8bc62fb60435029a84d1dd94eec613acbad3161b60fd827a3c111f2541c28079"
+  sha256 "2a915503548a6423e6ee813e6f993f9a219bacc6e7535a3544508cf46eb57b8e"
   license "MIT"
   disable! date: "2026-05-06", because: "renamed to Froth; install nikokozak/froth/froth"
 
