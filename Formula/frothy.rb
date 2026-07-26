0e23542f7bc547bbc36422a9ac71f48b14e73051
Niko Kozak <nikokozak@gmail.com>
2026-07-26 11:32:07 -0400

    publish Frothy v0.1.13

--- Formula/frothy.rb
+++ Formula/frothy.rb
@@ -5,16 +5,16 @@
 class Frothy < Formula
   desc "Live language kernel CLI for programmable devices"
   homepage "https://frothy.dev"
-  url "https://github.com/nikokozak/frothy/archive/refs/tags/v0.1.12.tar.gz"
-  sha256 "cb4c1d3173b5d9907de728818a2c75384568101505eea7e3351f52f30989aae5"
+  url "https://github.com/nikokozak/frothy/archive/refs/tags/v0.1.13.tar.gz"
+  sha256 "2cf9a16eb95be95b6d69752f08b652da00720121fb8f284388e5ede3b6efdf71"
   license "MIT"
 
   depends_on "go" => :build
   depends_on "esptool"
 
   resource "firmware" do
-    url "https://github.com/nikokozak/frothy/releases/download/v0.1.12/frothy-firmware-v0.1.12.tar.gz"
-    sha256 "1e8c64ec180c7951b97374ed37f3552d81f2254dd9009404503fec80da369e48"
+    url "https://github.com/nikokozak/frothy/releases/download/v0.1.13/frothy-firmware-v0.1.13.tar.gz"
+    sha256 "1a13c07bb4bb197bc4e19e8cc3a3dff1696b278ef4be729cd497b793ceeeeadd"
   end
 
   def install
